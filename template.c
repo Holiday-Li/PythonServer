9a71b9b58419a3322fb029a0a9e258eeccc76e44
Li Chunguang <guang99lee@126.com>
2024-06-05 09:45:08 +0800

    Add template file for IAR build function

---
+++ template.c
@@ -0,0 +1,95 @@
+/**
+ * Copyright Statement:
+ * This software and related documentation (ESWIN SOFTWARE) are protected under relevant copyright laws.
+ * The information contained herein is confidential and proprietary to
+ * Beijing ESWIN Computing Technology Co., Ltd.(ESWIN)and/or its licensors.
+ * Without the prior written permission of ESWIN and/or its licensors, any reproduction, modification,
+ * use or disclosure Software, and information contained herein, in whole or in part, shall be strictly prohibited.
+ * 
+ * Copyright ©[2023] [Beijing ESWIN Computing Technology Co., Ltd.]. All rights reserved.
+ * 
+ * RECEIVER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES THAT THE SOFTWARE
+ * AND ITS DOCUMENTATIONS (ESWIN SOFTWARE) RECEIVED FROM ESWIN AND / OR ITS REPRESENTATIVES
+ * ARE PROVIDED TO RECEIVER ON AN "AS-IS" BASIS ONLY. ESWIN EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON INFRINGEMENT.
+ * NEITHER DOES ESWIN PROVIDE ANY WARRANTY WHATSOEVER WITH RESPECT TO THE SOFTWARE OF ANY THIRD PARTY
+ * WHICH MAY BE USED BY,INCORPORATED IN, OR SUPPLIED WITH THE ESWIN SOFTWARE,
+ * AND RECEIVER AGREES TO LOOK ONLY TO SUCH THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO.
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ESWIN BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
+ * @file slt.c
+ * @brief 
+ * @author abu (abu@eswincomputing.com)
+ * @date 2024-03-19
+ * 
+ * @details
+ * 
+ * @platform
+ * 
+ * @pins
+ * | function name | port number | function number | board connector |
+ * @connections
+ * 
+ * @attention
+ * 
+ * Modification History :
+ * Date:               Version:                    Author:     
+ * Changes: 
+ * 
+ */
+
+#include "../board/sdk_project_config.h"
+#include "../include/led.h"
+#include "../include/can.h"
+#include "../include/timer.h"
+#include "../include/uart.h"
+#include "../include/modules_test.h"
+#include "../include/command.h"
+#include "log.h"
+/* Added by Lichg 20240424 */
+#include "osal.h"
+#include "../include/task.h"
+#include "../modules/modules.h"
+
+#include <stdio.h>
+#include <string.h>
+
+volatile int g_exitCode = 0;
+
+static inline void ProcByCmd(void *param)
+{
+    doMsgCommand();
+}
+
+static inline void ProcBySeq(void)
+{
+    doModuleCaseBySeq();
+}
+
+/**
+ * @brief The main function for the project.
+ *
+ */
+int demos_main(void)
+{
+    CLOCK_SYS_Init(g_pstClockManGcConfigsArr[0]);
+
+    /* Init the task queue */
+    TaskInit();
+    OS_ASSERT(TASK_SUCCESS == TaskListAdd(&%task_name%));
+
+    /* Main loop */
+    while (0 == g_exitCode) {
+        ProcBySeq();
+        // ProcByCmd(NULL);
+    }
+
+    return 0;
+}
