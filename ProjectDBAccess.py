43f2eb25ea44951bf5f3d64501cb6e919d8eba3c
Li Chunguang <guang99lee@126.com>
2024-06-11 15:53:24 +0800

    DBAccess: Update database access function

--- ProjectDBAccess.py
+++ ProjectDBAccess.py
@@ -1,123 +1,223 @@
 import os
-import json
+import pyodbc
+from Utils import get_current_dir_path
 import logging
-from ProjectAnalysis import get_project_name, get_core_arch, get_ewp_file_path, \
-    get_compiler_downloader_path, get_project_test_cases
+from Logger import get_logger
 
-class DBAccesser:
-    def __init__(self):
-        # TBD, init the database access object
-        # Using file replace database now.
-        current_path = os.path.dirname(os.path.abspath(__file__))
-        self.data_file_path = os.path.join(current_path, "project_info.txt")
+def connect_database(logger:logging.Logger, mdb_file_name:str)->tuple[pyodbc.Connection, pyodbc.Cursor]:
+    # Get mdb file path
+    base_path = get_current_dir_path()
+    mdb_file = os.path.join(base_path, mdb_file_name)
+    logger.info("MDB file path:\n{}".format(mdb_file))
 
-    def __get_project_list(self):
-        project_list = []
-        if os.path.exists(self.data_file_path):
-            with(open(self.data_file_path, "r")) as file:
-                data = file.read()
-                if len(data) == 0:
-                    project_list = []
-                else:
-                    data_list = json.loads(data)
-                    project_list = sorted(data_list, key=lambda data_list:data_list["id"])
-        return project_list
+    if not os.path.exists(mdb_file):
+        logger.error("MDB file not exist")
+        return None, None
 
-    def __is_need_update(self, project_list, project_path):
-        update_flag = True
-        for project_info in project_list:
-            if project_info["project_path"] == project_path:
-                update_flag = False
-                break
-        return update_flag
+    driver = "{Microsoft Access Driver (*.mdb, *.accdb)}"
+    conn = pyodbc.connect(f"driver={driver};DBQ={mdb_file}")
+    cursor = conn.cursor()
+    return conn, cursor
 
-    def __get_project_info(self, project_path, max_index):
-        project_info = {}
-        project_name = get_project_name(project_path=project_path)
-        if not project_name:
-            return None
-        print("ProjectName:{}".format(project_name))
 
-        ewp_path = get_ewp_file_path(project_path=project_path)
-        if not ewp_path:
-            return None
-        print("ewpFilePath:{}".format(ewp_path))
-        core_arch = get_core_arch(project_file_path=ewp_path)
-        if not core_arch:
-            return None
-        print("CoreArch:{}".format(core_arch))
+def close_database(conn:pyodbc.Connection, cursor:pyodbc.Cursor):
+    cursor.close()
+    conn.close()
+    return
 
-        compiler_path, downloader_path = get_compiler_downloader_path(project_path=project_path,
-                                                                      core_arch=core_arch,
-                                                                      project_name=project_name)
-        if (not compiler_path) or (not downloader_path):
-            return None
-        print("compilerPath:{}".format(compiler_path))
-        print("downloaderPath:{}".format(downloader_path))
-        
-        test_cases = get_project_test_cases(project_path=project_path)
 
-        project_info["id"] = max_index + 1
-        project_info["name"] = project_name
-        project_info["project_path"] = project_path
-        project_info["ewp_path"] = ewp_path
-        project_info["core_arch"] = core_arch
-        project_info["compiler"] = compiler_path
-        project_info["downloader"] = downloader_path
-        project_info["test_cases"] = test_cases
-        return project_info
-
-    def update_project_info(self, project_path):
-        print("ProjectPath:\n{}".format(project_path))
-        project_list = self.__get_project_list()
-        print("Len of project_list:{}".format(len(project_list)))
-        
-        if not self.__is_need_update(project_list, project_path):
-            return "Done"
-
-        if not project_list:
-            max_index = 0
-        else:
-            max_index = project_list[-1]["project_id"]
-        project_info = self.__get_project_info(project_path, max_index)
-        if not project_info:
-            print("Could not found project")
-            return "Done"
-
-        project_list.append(project_info)
-        with(open(self.data_file_path, "w+")) as file:
-            data = json.dumps(project_list, indent=4)
-            file.write(data)
-        return "Done"
-
-    def get_project_info(self, project_id):
-        project_list = self.__get_project_list()
-        for project_info in project_list:
-            if project_info["id"] == project_id:
-                return project_info
+def get_project_path(project_id:int, logger:logging.Logger, mdb_file_name:str="caseManage .mdb")->str:
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
         return None
-    
-    def del_project_info(self, project_id):
-        project_list = self.__get_project_list()
-        for project_info in project_list:
-            if project_info["id"] == project_id:
-                project_list.remove(project_info)
-                break
 
-        with(open(self.data_file_path, "w+")) as file:
-            data = json.dumps(project_list, indent=4)
-            file.write(data)
-        return
+    sql = "SELECT ProjectPath FROM ProjectInformation WHERE ID={}".format(project_id)
+    try:
+        cursor.execute(sql)
+    except:
+        logger.error("Get project path, SQL excute error.")
+        close_database(conn=conn, cursor=cursor)
+        return None
+
+    rows = cursor.fetchall()
+    if len(rows) != 1:
+        logger.error("Get project path error, result number:{}".format(len(rows)))
+        close_database(conn=conn, cursor=cursor)
+        return None
+    project_path = rows[0][0]
+
+    close_database(conn=conn, cursor=cursor)
+    return project_path
+
+
+def get_ide_path(project_id:str, logger: logging.Logger, mdb_file_name:str="caseManage .mdb")->str:
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return None
+
+    sql = "SELECT idePath FROM ProjectInformation WHERE ID={}".format(project_id)
+    try:
+        cursor.execute(sql)
+    except:
+        logger.error("Get IDE path error, result number:{}".format(len(rows)))
+        close_database(conn=conn, cursor=cursor)
+        return None
+
+    rows = cursor.fetchall()
+    if len(rows) != 1:
+        logger.error("Get IDE path error, result number:{}".format(len(rows)))
+        close_database(conn=conn, cursor=cursor)
+        return None
+    ide_path = rows[0][0]
+
+    close_database(conn=conn, cursor=cursor)
+    return ide_path
+
+
+
+def add_new_colum(table_name:str, colum_name:str, colum_type:str,
+                  logger:logging.Logger, mdb_file_name:str="caseManage .mdb")->bool:
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return False
+
+    sql = "ALTER TABLE {} ADD COLUMN {} {};".format(table_name, colum_name, colum_type)
+    try:
+        cursor.execute(sql=sql)
+        conn.commit()
+    except:
+        logger.error("SQL execute error. SQL:{}".format(sql))
+        close_database(conn=conn, cursor=cursor)
+        return False
+
+    close_database(conn=conn, cursor=cursor)
+    return True
+
+
+def get_colums_name(table_name:str, logger:logging.Logger, mdb_file_name:str="caseManage .mdb") -> list:
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return None
+
+    try:
+        columns = cursor.columns(table=table_name)
+    except:
+        logger.error("Get columns error.")
+        close_database(conn=conn, cursor=cursor)
+        return None
+
+    columns_name = []
+    for column in columns:
+        columns_name.append(column.column_name)
+
+    close_database(conn=conn, cursor=cursor)
+    return columns_name
+
+
+def show_table_column(table_name:str, logger:logging.Logger):
+    columns_name = get_colums_name(table_name=table_name, logger=logger)
+    print("TableName:{}".format(table_name))
+    for column_name in columns_name:
+        print("\tColumnName:{}".format(column_name))
+    return
+
+
+def add_new_line_for_project_info(mdb_file_name:str="caseManage .mdb"):
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return False
+
+    sql = "INSERT INTO ProjectInformation "\
+          "(ID, ProjectName, ProjectPath, EwpPath, CoreArch, Compiler, DownLoader, SourcePath, idePath)" \
+          "VALUES" \
+          "(5, 'slt_test', 'F:\\Work\\IAR_WorkSpace\\emps_SLT_demo_20240402', '', '', '', '', '', 'F:\\Programs\\IAR Systems\\Embedded Workbench 9.2')"
+    logger.info("SQL:\n\t{}".format(sql))
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except:
+        logger.error("SQL execute error.")
+        close_database(conn=conn, cursor=cursor)
+        return False
+
+    close_database(conn=conn, cursor=cursor)
+    return True
+
+
+def show_table_info_table(table_name:str, mdb_file_name:str="caseManage .mdb"):
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return False
+
+    sql="SELECT * FROM {}".format(table_name)
+    try:
+        cursor.execute(sql=sql)
+    except:
+        logger.error("SQL executes error, SQL:{}".format(sql))
+        close_database(conn=conn, cursor=cursor)
+        return False
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    close_database(conn=conn, cursor=cursor)
+    return True
+
+
+def get_node_name(module_id:str, sub_id:str, logger:logging.Logger, mdb_file_name:str="caseManage .mdb")->str:
+    conn, cursor = connect_database(logger=logger, mdb_file_name=mdb_file_name)
+    if not conn or not cursor:
+        logger.error("Connect database error")
+        return None
+
+    sql = "SELECT NodName FROM TestCase WHERE MoubleID=? and SubID=?"
+    try:
+        cursor.execute(sql, (module_id, sub_id))
+    except:
+        logger.error("Execute SQL error")
+        close_database(conn=conn, cursor=cursor)
+        return None
+    rows = cursor.fetchall()
+    if len(rows) != 1:
+        logger.error("Get node name error, get item: {}".format(len(rows)))
+        close_database(conn=conn, cursor=cursor)
+        return None
+
+    node_name = rows[0][0]
+    close_database(conn=conn, cursor=cursor)
+    return node_name
 
 
 if __name__ == "__main__":
-    project_path = ("C:\\Users\\lichunguang\\Documents\\WorkSpace\\"
-                        "IAR_WorkSpace\\emps_SLT_demo_20240402")
-    db_accesser = DBAccesser()
-    db_accesser.update_project_info(project_path)
+    logger = get_logger("mdb_test")
+    # '''
+    table_name = "ProjectInformation"
+    print("------------{} Test------------".format(table_name))
+    show_table_column(table_name=table_name, logger=logger)
+    # add_new_line_for_project_info()
+    # show_table_info_table(table_name=table_name)
+    print("Test: get_project_path")
+    project_path = get_project_path(project_id=5, logger=logger)
+    print("\tProjectPath: {}".format(project_path))
+    print("\tProjectPathType: {}".format(type(project_path)))
+    print("Test: get_ide_path")
+    ide_path = get_ide_path(project_id=5, logger=logger)
+    print("\tidePath: {}".format(ide_path))
+    print("\tidePathType: {}".format(type(ide_path)))
+    # '''
 
-    project_info = db_accesser.get_project_info(project_id=1)
-    project_info_str = json.dumps(project_info, indent=4)
-    print("ProjectInfo:\n{}".format(project_info_str))
-
-    db_accesser.del_project_info(project_id=1)
+    table_name = "TestCase"
+    print("------------{} Test------------".format(table_name))
+    show_table_column(table_name=table_name, logger=logger)
+    # show_table_info_table(table_name=table_name)
+    print("Get node name:")
+    node_name = get_node_name(module_id="MODULE_ID_SPI", sub_id="0x0001", logger=logger)
+    print("\tNodeName:{}".format(node_name))
+    del logger
