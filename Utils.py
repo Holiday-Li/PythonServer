a893b2b25fc03c6bcc7d9e631d976db682eeb5d5
Li Chunguang <guang99lee@126.com>
2024-06-13 16:34:30 +0800

    Utils: Add get_config_info function

--- Utils.py
+++ Utils.py
@@ -1,4 +1,4 @@
-import os
+import os, logging
 
 '''
  * Function name: get_current_dir_path
@@ -11,17 +11,26 @@ def get_current_dir_path():
     dir_path = os.path.dirname(current_file_path)
     return dir_path
 
-def clean_log_files():
-    base_path = get_current_dir_path()
-    log_folder = os.path.join(base_path, "log\\")
-    print("log folder path:{}".format(log_folder))
-    print("log files:")
-    for root, _, files in os.walk(log_folder):
-        for file in files:
-            log_file = os.path.join(root, file)
-            print("\t{}".format(log_file))
-            os.remove(log_file)
-    return
+def get_config_info(logger:logging.Logger, config_file_path=None)->dict:
+    config_info = {}
+    if not config_file_path:
+        config_file_path = ".\\config.cfg"
+    if not os.path.exists(config_file_path):
+        logger.error("config file does not exist")
+        return None
+    with(open(config_file_path, "r")) as fp:
+        lines = fp.readlines()
+
+    for line in lines:
+        key, value = line.split("=")
+        print("Key:{}, Value:{}".format(key, value))
+        config_info[key]=value
+    return config_info
 
 if __name__ == "__main__":
+    from Logger import clean_log_files, get_logger
     clean_log_files()
+    logger = get_logger("utils_test")
+    config_info = get_config_info(logger=logger)
+    print("ConfigInfo: {}".format(config_info))
+    print("BasePath:{}".format(config_info["base_path"]))
