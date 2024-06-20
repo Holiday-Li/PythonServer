c575010975571ff6c3d34878b822b87fe2c7e398
Li Chunguang <guang99lee@126.com>
2024-06-20 14:46:44 +0800

    Utils: Optimization

--- Utils.py
+++ Utils.py
@@ -11,26 +11,48 @@ def get_current_dir_path():
     dir_path = os.path.dirname(current_file_path)
     return dir_path
 
-def get_config_info(logger:logging.Logger, config_file_path=None)->dict:
-    config_info = {}
+config_info = {}
+
+def get_config_info(logger:logging.Logger, config_file_path=None)->bool:
+    global config_info
     if not config_file_path:
         config_file_path = ".\\config.cfg"
     if not os.path.exists(config_file_path):
         logger.error("config file does not exist")
-        return None
+        return False
     with(open(config_file_path, "r")) as fp:
         lines = fp.readlines()
 
     for line in lines:
         key, value = line.split("=")
-        print("Key:{}, Value:{}".format(key, value))
-        config_info[key]=value
-    return config_info
+        # print("Key:{}, Value:{}".format(key, value))
+        config_info[key]=value.replace("\n", "")
+    return True
+
+def get_config_value(key:str, logger:logging.Logger)->str:
+    global config_info
+    if not config_info:
+        get_config_info(logger)
+    if key in config_info:
+        return config_info[key]
+    else:
+        logger.error("No key in config_info, please check the config file.")
+        logger.error("Key : {}".format(key))
+        return ""
+
+def get_code_base_path(logger:logging.Logger)->str:
+    key = "code_base_path"
+    return get_config_value(key=key, logger=logger)
+
+def get_db_base_path(logger:logging.Logger)->str:
+    key = "db_base_path"
+    return get_config_value(key=key, logger=logger)
 
 if __name__ == "__main__":
     from Logger import clean_log_files, get_logger
     clean_log_files()
     logger = get_logger("utils_test")
-    config_info = get_config_info(logger=logger)
-    print("ConfigInfo: {}".format(config_info))
-    print("BasePath:{}".format(config_info["base_path"]))
+    code_base = get_code_base_path(logger)
+    print("CodeBase:{}".format(code_base))
+    db_base = get_db_base_path(logger)
+    print("DB Base: {}".format(db_base))
